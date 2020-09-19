02e3ba6d74b75189dbbfb017dab8ee9e74741099
klaudijus <klaudijus.strategeens@gmail.com>
2020-09-19 21:26:39 +0700

    updated 'log is completed' styles

--- src/components/log-is-completed-message/LogIsCompletedMessage.js
+++ src/components/log-is-completed-message/LogIsCompletedMessage.js
@@ -10,22 +10,22 @@ function LogIsCompletedMessage({date, onBack}) {
     return (
         <div className="log-is-completed-message">
             <FormDate date={date}/>
-            <div className="log-is-completed-message__illustration">
-                <CheckCircleOutlineIcon/>
-            </div>
             <div className="log-is-completed-message__congratulations">
+                <div className="log-is-completed-message__illustration">
+                    <CheckCircleOutlineIcon/>
+                </div>
                 Your day has been logged !
             </div>
             <div className="message-navigation-options">
                 <div className='message-navigation-options__to-form'>
-                    <div>If you have missed something</div>
                     <Button className="app-button app-button--outline message-navigation-options__button" variant="outlined" size="small" onClick={() => onBack()}>Back to logging</Button>
+                    <div>If you have missed something</div>
                 </div>
                 <div className='message-navigation-options__to-calendar'>
-                    <div>To review overall statistics</div>
                     <Link to="/calendar">
                         <Button className="app-button app-button--outline message-navigation-options__button" variant="outlined" size="small">Go to calendar</Button>
                     </Link>
+                    <div>To review overall statistics</div>
                 </div>
             </div>
         </div>
